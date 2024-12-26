c62c8c4be9770b4813d34e2e0b35068897f43826
Raidenvh <raiden.422024009@civitas.ukrida.ac.id>
2024-12-26 23:17:25 +0700

    Create Basic.py

---
+++ Basic.py
@@ -0,0 +1,21 @@
+class Basic:
+    def huffman_coding(self, data):
+        """
+        Implementation of Huffman Coding algorithm.
+        :param data: Input data to encode
+        """
+        # TODO: Add implementation of Huffman Coding
+        pass
+
+    def euclids_algorithm(self, a, b):
+        """
+        Implementation of Euclid's Algorithm to find the GCD.
+        :param a: First integer
+        :param b: Second integer
+        :return: GCD of a and b
+        """
+        while b:
+            a, b = b, a % b
+        return a
+
+    # Add other algorithms here
