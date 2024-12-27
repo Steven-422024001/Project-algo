9c6ff95be39d4ae6e9a31a0f33e7a856b2c12066
Raidenvh <raiden.422024009@civitas.ukrida.ac.id>
2024-12-27 12:51:48 +0700

    Basic.py

--- Basic.py
+++ Basic.py
@@ -1,21 +1,33 @@
 class Basic:
     def huffman_coding(self, data):
         """
-        Implementation of Huffman Coding algorithm.
-        :param data: Input data to encode
+        Huffman Coding: Menghasilkan kode biner untuk setiap karakter berdasarkan frekuensi.
         """
-        # TODO: Add implementation of Huffman Coding
-        pass
+        # Contoh sederhana: karakter yang sering muncul diberi kode lebih pendek.
+        from collections import Counter
+        freq = Counter(data)
+        return {char: bin(i)[2:] for i, char in enumerate(freq)}
 
     def euclids_algorithm(self, a, b):
         """
-        Implementation of Euclid's Algorithm to find the GCD.
-        :param a: First integer
-        :param b: Second integer
-        :return: GCD of a and b
+        Euclid's Algorithm: Mencari FPB (GCD) dari dua angka.
         """
-        while b:
+        while b != 0:
             a, b = b, a % b
         return a
 
-    # Add other algorithms here
+    def union_find(self, n, edges):
+        """
+        Union-Find: Menentukan jumlah kelompok (komponen) dalam sebuah graf.
+        """
+        parent = list(range(n))
+
+        def find(x):
+            while x != parent[x]:
+                x = parent[x]
+            return x
+
+        for u, v in edges:
+            parent[find(u)] = find(v)
+
+        return len(set(find(i) for i in range(n)))
